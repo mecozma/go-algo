264da0fd11abdb5f1ba85befbda43408eeda09bf
Eduard <mecozma@gmail.com>
2023-12-01 14:22:59 +0200

    Added the Push method for the JS SLL.

--- singly-linked-lists/singly-linked-list.js
+++ singly-linked-lists/singly-linked-list.js
@@ -11,4 +11,16 @@ class SinglyLinkedList {
         this.tail = null;
         this.length = 0;
     }
+    Push(val) {
+        var newNode = new Node(val);
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
 }
