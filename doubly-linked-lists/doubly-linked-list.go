ad6365c4d1fd60b739414a8b3fa942710f6aebba
Eduard <mecozma@gmail.com>
2023-12-10 21:05:42 +0200

    Implemented Push and Traverse methods in Go and JS.

--- doubly-linked-lists/doubly-linked-list.go
+++ doubly-linked-lists/doubly-linked-list.go
@@ -1,17 +1,57 @@
 package main
 
+import "fmt"
+
 type Node struct {
 	value int
 	next  *Node
 	prev  *Node
 }
 
-type List struct {
+type DoublyLinkedList struct {
 	head   *Node
 	tail   *Node
 	length int
 }
 
+// Traverse method prints all the nodes of a dll.
+func (l *DoublyLinkedList) Traverse() {
+	if l.head == nil {
+		fmt.Println("No nodes!")
+		return
+	}
+
+	first := l.head
+
+	for {
+		fmt.Println(first.value)
+		first = first.next
+		if first == nil {
+			break
+		}
+	}
+}
+
+// Push method takes a value as argument and adds it as a node at the end of dll.
+func (l *DoublyLinkedList) Push(n int) *DoublyLinkedList {
+	newNode := &Node{value: n}
+	if l.head == nil {
+		l.head = newNode
+	} else {
+		l.tail.next = newNode
+		newNode.prev = l.tail
+	}
+	l.tail = newNode
+	l.length++
+	return l
+}
+
 func main() {
+	dll := &DoublyLinkedList{}
+
+	dll.Push(0)
+	dll.Push(1)
+
+	dll.Traverse()
 
 }
--- doubly-linked-lists/doubly-linked-list.js
+++ doubly-linked-lists/doubly-linked-list.js
@@ -16,4 +16,39 @@ class DoublyLinkedList {
         this.tail = null;
         this.length = 0;
     }
+
+    // Traverse method prints all the nodes of a dll.
+    Traverse() {
+        if (this.head == null) {
+            return "No nodes";
+        }
+
+        var start = this.head;
+        while (start) {
+            console.log(start.val);
+            start = start.next;
+            if (start == null) {
+                break;
+            }
+        }
+    }
+
+    // Push method takes a value as argument and adds it as a node at the end of dll.
+    Push(val) {
+        var newNode = new Node(val);
+        if (this.length === 0) {
+            this.head = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+        }
+        this.tail = newNode;
+        this.length++;
+        return this;
+    }
 }
+
+var dll = new DoublyLinkedList()
+dll.Push(0)
+dll.Push(1)
+console.log(dll.Traverse())
